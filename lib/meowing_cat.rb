904552c4abd39cd9b90b2139640eef0c26f34aaf
hortencia c <hortenciaecisneros@gmail.com>
2020-08-05 15:14:32 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,21 @@
+# require 'pry
+
+class Cat 
+attr_accessor:name
+
+ def meow
+   puts "meow!"
+ end
+
+
+end
+
+# notes to self make sure you are wrtting everything as required , if asked !! then inculde wasted too much time on this !
+#  create a class for cat 
+# next create your attr_accessor to access your instance name ! 
+# define "meow" and "puts" it out = "meow!". end ,end 
+
+
+
 ## code your solution here. 
+# binding.pry
